1081124b9d7bf96e64d296afb3656c17ee19434e
Fran López <franlopezpineda@gmail.com>
2026-05-15 14:01:22 +0200

    Fix: Remove findByTipo from ComidaRepository (fixes boot crash)

--- backend/src/main/java/com/backend/fitlife/persistence/repositories/ComidaRepository.java
+++ backend/src/main/java/com/backend/fitlife/persistence/repositories/ComidaRepository.java
@@ -8,8 +8,7 @@ import com.backend.fitlife.persistence.entities.Comida;
 
 public interface ComidaRepository extends JpaRepository<Comida, Integer>{
 
-	// Buscar comidas por tipo(desayuno,almuerzo etc)
-	List<Comida> findByTipo(String tipo);
+
 	
 	// Buscar comidas que contengan un nombre similar
 	List<Comida> findByNombreContainingIgnoreCase(String nombre);
