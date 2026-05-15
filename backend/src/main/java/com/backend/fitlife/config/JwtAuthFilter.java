554c26c7ef68939640a82b1cc1140aa9eff41a61
Fran López <franlopezpineda@gmail.com>
2026-05-15 13:53:15 +0200

    Fix syntax error in JwtAuthFilter (moved @Component)

--- backend/src/main/java/com/backend/fitlife/config/JwtAuthFilter.java
+++ backend/src/main/java/com/backend/fitlife/config/JwtAuthFilter.java
@@ -7,7 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-@Component
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.backend.fitlife.services.CustomUserDetailsService;
@@ -17,6 +17,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Autowired
